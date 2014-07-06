31b96c11ee7aace14e5ef6512e4969c3e286ed02
thumbert <humbert.tony@yahoo.com>
2014-07-06 14:37:40 -0400

    toilets

--- dart/demos/lib/math/combinatorics/pentagrams.dart
+++ dart/demos/lib/math/combinatorics/pentagrams.dart
@@ -366,7 +366,22 @@ class Matrix<A> {
 
 main() {
  
-  
+  // see the tests
+  // http://oeis.org/  found it!
+//  order    number of Ngrams
+//  1        1
+//  2        1
+//  3        2
+//  4        5
+//  5        12
+//  6        35
+//  7        108
+//  8        369
+//  9        1285
+//  10       4655
+//  11       17073
+//  12       63600
+//  
   
 }
 
