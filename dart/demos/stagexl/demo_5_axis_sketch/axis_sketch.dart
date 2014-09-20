0e538b7eee585c15dc2ddd76fba638fd721f1019
thumbert <humbert.tony@yahoo.com>
2014-09-20 18:43:43 -0400

    center that label

--- dart/demos/stagexl/demo_5_axis_sketch/axis_sketch.dart
+++ dart/demos/stagexl/demo_5_axis_sketch/axis_sketch.dart
@@ -7,63 +7,80 @@ Stage stage = new Stage(html.querySelector('#stage'));
 
 RenderLoop renderLoop = new RenderLoop();
 
+addGrid() {
+  var grid = new Shape();
+  
+  num h = 0;
+  while( h < stage.stageHeight) {
+    grid.graphics.moveTo(0, h);
+    grid.graphics.lineTo(stage.stageWidth, h);
+    h += 100;
+  }
+  num v = 0;
+  while( v < stage.stageWidth) {
+    grid.graphics.moveTo(v, 0);
+    grid.graphics.lineTo(v,stage.stageHeight);
+    v += 100;
+  }
+  grid.graphics.strokeColor(Color.Gray, 1);
+  
+  stage.addChild( grid );
+}
+
+
+
 class Direction {
-  static const int UP   = 1;
+  static const int UP = 1;
   static const int DOWN = 2;
-  static const int LEFT   = 1;
+  static const int LEFT = 1;
   static const int RIGHT = 2;
 }
 
 
 class Tick extends DisplayObjectContainer {
-  
+
   Shape line;
   TextField textField;
-  
-  Tick(int direction, 
-       int length, 
-       int padding, 
-       String text) {
+
+  Tick(int length, int padding, String text) {
     line = new Shape();
-    if (direction == Direction.DOWN) {
-      line.graphics.moveTo(0, 0);
-      line.graphics.lineTo(0, length);
-      var fmt = new TextFormat("Arial", 14, Color.Black);
-      textField = new TextField(text, fmt)
-        ..y = length + padding;
-    } else if (direction == Direction.UP) {
-      line.graphics.lineTo(0, -length);      
-    } else if (direction == Direction.RIGHT) {
-      line.graphics.lineTo(length,0);
-    } else if (direction == Direction.LEFT) {
-      line.graphics.lineTo(-length,0);      
-    }
+    line.graphics.moveTo(0, 0);
+    line.graphics.lineTo(0, length);
+    var fmt = new TextFormat("Arial", 14, Color.Black, align: TextFormatAlign.CENTER);
+    textField = new TextField()
+        ..defaultTextFormat = fmt
+        ..y = length + padding
+        ..autoSize = TextFieldAutoSize.CENTER  
+        ..text = text;
+    textField..x = -textField.width ~/ 2;
     line.graphics.strokeColor(Color.Black, 1);
-    
+
     addChild(line);
     addChild(textField);
   }
-  
+
 }
 
 class Axis extends DisplayObjectContainer {
-  
-  
-  
+
+
+
 }
 
 
-main(){
+main() {
   var renderLoop = new RenderLoop();
   renderLoop.addStage(stage);
 
   stage.backgroundColor = Color.Beige;
-  
-  var tick1 = new Tick(Direction.DOWN, 20, 5, "Text")
-    ..x = 100
-    ..y = 100;
-  
-  stage.addChild(tick1);
+  addGrid();
   
   
+  var tick = new Tick(20, 5, "A really superlong Text")
+      ..x = 100
+      ..y = 100;
+
+  stage.addChild(tick);
+
+
 }
