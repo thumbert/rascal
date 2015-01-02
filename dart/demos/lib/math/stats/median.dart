98f314a9b589cbbbb27738b0e0d9b3832f104f5f
thumbert <humbert.tony@yahoo.com>
2015-01-01 22:03:21 -0500

    nothing really

--- dart/demos/lib/math/stats/median.dart
+++ dart/demos/lib/math/stats/median.dart
@@ -1,30 +1,28 @@
-library stats.median;
+library math.stats.median;
 
 class Quantile {
 
   List<Comparable> x;
   
-  Quantile();
+  Quantile(this.x);
   
-  List swap(List a, int i, int j) {
-    var t = a[i];
-    a[i] = a[j];
-    a[j] = t;
-    return a;
+  swap(int i, int j) {
+    var t = x[i];
+    x[i] = x[j];
+    x[j] = t;
   }
 
-
-  int partition(List<Comparable> a, int lo, int hi) {
+  int partition(int lo, int hi) {
     int i = lo,
         j = hi + 1;
-    var v = a[lo];
+    var v = x[lo];
     while (true) {
-      while (a[++i].compareTo(v) < 0) if (i == hi) break;
-      while (v.compareTo(a[--j]) < 0) if (j == lo) break;
+      while (x[++i].compareTo(v) < 0) if (i == hi) break;
+      while (v.compareTo(x[--j]) < 0) if (j == lo) break;
       if (i >= j) break;
-      swap(a, i, j);
+      swap(i, j);
     }
-    swap(a, lo, j);
+    swap(lo, j);
     return j;
   }
   
@@ -32,18 +30,19 @@ class Quantile {
     
   }
 
-  _sortLoHi(List<Comparable> a, int lo, int hi) {
-    if (hi <= lo) return a;
-    int j = partition(a, lo, hi);
-    _sortLoHi(a, lo, j-1);
-    _sortLoHi(a, j+1, hi);
+  _sortLoHi(int lo, int hi) {
+    if (hi <= lo) return;
+    int j = partition(lo, hi);
+    _sortLoHi(lo, j-1);
+    _sortLoHi(j+1, hi);
   }
   
-  sort(List<Comparable> a) {
-    return _sortLoHi(a, 0, a.length-1);
-  }
+  sort() => _sortLoHi(0, x.length-1);
+  
   
 }
+
+
 
 
 /**
--- dart/demos/test/math/stats/test_median.dart
+++ dart/demos/test/math/stats/test_median.dart
@@ -1,24 +1,36 @@
 library test_median;
 
 import 'package:demos/math/stats/median.dart';
-import 'package:unittest/unittest.dart';
+
+//import 'package:unittest/unittest.dart';
+//import 'package:demos/math/stats/median.dart';
 
 main() {
-  print((22) >> 1);
-  
+  print("Hi");
+
   List x = 'KRATELEPUIMQCXOS'.split("");
   //x.sort();
   print(x);
   
-  Quantile q = new Quantile();
-  //print(q.partition(x, 0, x.length-1));
-  //print(q.x);
   
-  print(q.sort(x));
+  Quantile q = new Quantile(x);
+  q.sort();
+  print(q.x);
   
-  
-  
-  test("median", () {
-    //expect(quickSelect([5,4,2,1,3], 3), 3);
-  });
 }
+
+//other() {
+//  print((22) >> 1);
+//  
+//  
+//  //print(q.partition(x, 0, x.length-1));
+//  //print(q.x);
+//  
+//  //print(q.sort());
+//  
+//  
+//  
+////  test("median", () {
+////    expect(quickSelect([5,4,2,1,3], 3), 3);
+////  });
+//}
