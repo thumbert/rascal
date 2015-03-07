77983d583ce0a6a1a8098220d0d4cbbf9b52a185
thumbert <humbert.tony@yahoo.com>
2015-03-07 16:11:50 -0500

    more and better

--- dart/demos/stagexl/demo_7_containers/container.dart
+++ dart/demos/stagexl/demo_7_containers/container.dart
@@ -10,87 +10,123 @@ import 'package:stagexl/stagexl.dart';
 
 Stage stage = new Stage(html.querySelector('#stage'));
 RenderLoop renderLoop = new RenderLoop();
+TextField tooltip;
+
 
 class PlotArea extends DisplayObjectContainer {
-   
+
   PlotArea(int width, int height) {
     print("Plot area width={$width} and heigth={$height}");
     Shape background = new Shape()
-       ..width = width
-       ..height = height
-       ..graphics.rect(0, 0, width, height)    
-       ..graphics.strokeColor(Color.Black, 1.5, JointStyle.MITER)
-       ..graphics.fillColor(Color.White);
-     addChild(background);   
+      ..width = width
+      ..height = height
+      ..graphics.rect(0, 0, width, height)
+      ..graphics.strokeColor(Color.Black, 1.5, JointStyle.MITER)
+      ..graphics.fillColor(Color.White);
+    addChild(background);
   }
-  
+
 }
 
 class Line extends Sprite {
   List<num> xData;
   List<num> yData;
   int color;
-  String seriesName;
-  TextField tooltip;
-  
-  Line(this.xData, this.yData, {this.color: Color.Tomato, this.seriesName}) {
-    draw();
-    graphics.strokeColor(color, width=1.5);    
-    tooltip = new TextField( seriesName )
-      ..x=250
-      ..y=50
-      ..alpha = 0;
-    stage.addChild(tooltip);
-    
-    onMouseOver.listen( _onMouseOver );
-    onMouseOut.listen( _onMouseOut );
+
+  Line(this.xData, this.yData, {this.color: Color.Tomato}) {
+    onMouseOver.listen(_onMouseOver);
+    onMouseOut.listen(_onMouseOut);
+    onMouseWheel.listen(_onMouseWheel);
+
   }
-  
-  draw() {
+
+  draw({num strokeWidth: 1.5}) {
     graphics.moveTo(xData.first, yData.first);
-    for (int i=1; i<xData.length; i++) {
-      graphics.lineTo(xData[i], yData[i]);      
+    for (int i = 1; i < xData.length; i++) {
+      graphics.lineTo(xData[i], yData[i]);
     }
+    graphics.strokeColor(color, width = strokeWidth);
   }
-  
-  _onMouseOver( MouseEvent e) {
-    tooltip.x = stage.mouseX + 15;
-    tooltip.y = stage.mouseY - 15;
-    tooltip.alpha = 1;
+
+  _onMouseWheel(MouseEvent e) {
+    print('Wheelling!');
+    //parent.setChildIndex(this, 0);  // push this at the bottom, not good
+
+    var point = new Point(e.localX, e.localY);
+    //var list = this.getObjectsUnderPoint(point);
+
+
+//    var child = parent.getChildAt(parent.numChildren - 1);  // the one on top
+//    print(child.name);
+//    tooltip.text = child.name;
+//    tooltip.x = parent.mouseX + 15;
+//    tooltip.y = parent.mouseY - 15;
+//    tooltip.alpha = 1;
+//    parent.setChildIndex(tooltip, parent.numChildren - 1); // put the tooltip in the forefront
+  }
+
+  _onMouseOver(MouseEvent e) {
     graphics.clear();
-    draw();
-    graphics.strokeColor(color, width=5);    
-    parent.setChildIndex(this, parent.numChildren-1);  // put this line in the forefront
+    draw(strokeWidth: 5);
+    parent.setChildIndex(this, parent.numChildren - 1); // put this line in front of all lines
+
+    tooltip.text = name;
+    tooltip.x = e.localX + 15;
+    tooltip.y = e.localY - 15;
+    tooltip.alpha = 1;
+    parent.setChildIndex(tooltip, parent.numChildren - 1); // put the tooltip in the forefront
+
   }
-  _onMouseOut( MouseEvent e) {
+
+  _onMouseOut(MouseEvent e) {
     tooltip.alpha = 0;
     graphics.clear();
-    draw();
-    graphics.strokeColor(color, width=1.5);
-  }  
-  
+    draw(strokeWidth: 1.5);
+  }
+
 }
 
 void main() {
   //stage has width=800, height=500 
   renderLoop.addStage(stage);
-  stage.backgroundColor = Color.AliceBlue;
+  stage.backgroundColor = Color.Beige;
 
-  List xData = new List.generate(7, (i) => 50 + 100*i);
+  List xData = new List.generate(7, (i) => 50 + 100 * i);
   List y1Data = [50, 150, 300, 375, 325, 275, 315];
-  List y2Data = [100, 250, 250, 305, 225, 255, 325];
-  List y3Data = [5, 200, 200, 225, 125, 155, 125];
-  
+  List y2Data = [100, 250, 250, 305, 225, 155, 125];
+  List y3Data = [5, 200, 200, 225, 225, 155, 125];
+  List y4Data = [25, 66, 30, 25, 225, 155, 125];
+
+
   var area = new PlotArea(700, 400)
     ..x = 50
     ..y = 50
+    ..name = 'PlotArea'
     ..addTo(stage);
-  
-  area.addChild( new Line(xData, y1Data, color: Color.Tomato, seriesName:"Series 1") );
-  area.addChild( new Line(xData, y2Data, color: Color.BlueViolet, seriesName:"Series 2") );
-  area.addChild( new Line(xData, y3Data, color: Color.CornflowerBlue, seriesName:"Series 3") );
-  
-   
+
+  tooltip = new TextField('')
+    ..name = 'Tooltip'
+    ..alpha = 0
+    ..addTo(area);                  // one tooltip for all lines
+
+  new Line(xData, y1Data, color: Color.Tomato)
+    ..name = "Series 1"
+    ..draw()
+    ..addTo(area);
+  new Line(xData, y2Data, color: Color.BlueViolet)
+    ..name = "Series 2"
+    ..draw()
+    ..addTo(area);
+  new Line(xData, y3Data, color: Color.CornflowerBlue)
+    ..name = "Series 3"
+    ..draw()
+    ..addTo(area);
+  new Line(xData, y4Data, color: Color.Chocolate)
+    ..name = "Series 4"
+    ..draw()
+    ..addTo(area);
+
+
 }
 
 
