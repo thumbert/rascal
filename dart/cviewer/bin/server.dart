6f9de62b91c409977381e702573371fb84c5100b
thumbert <humbert.tony@yahoo.com>
2015-03-09 22:26:30 -0400

    more bc

--- dart/cviewer/bin/server.dart
+++ dart/cviewer/bin/server.dart
@@ -29,29 +29,46 @@ listenForRequests(HttpServer _server) {
 }
 
 /**
- * Get the data.
+ * Get the data from mongo.
  * query = {'constraintName': ['KR-EXP', 'NHSC-I'],
  *          'start': new DateTime(2015,1,1),
  *          'end': new DateTime(2015,3,1) }
  *
  */
-Future<List<Map>> fromMongo(Map query) {
+Future<List<Map>> fromMongo(List<String> constraints, DateTime start, DateTime end) {
   SelectorBuilder sb = where;
 
+  if (start != null)
+    sb = sb.gte('start', start);
+  if (end != null)
+    sb = sb.gte('end', end);
+  if (constraints != null)
+    sb = sb.oneFrom('constraintName', constraints);
 
-  return ;
+  sb = sb.excludeFields(['_id']);
+
+  return bc.coll.find(sb).toList();
 }
 
 void handleGet(HttpRequest request) {
-  //String params = request.uri.queryParameters['q'];
+  Map params = request.uri.queryParameters;
   //print(params);
+  DateTime start, end;
+  List<String> constraints;
+
+  if (params.containsKey('constraints'))
+    constraints = params['constraints'].split('|');
+  if (params.containsKey('start'))
+    start = DateTime.parse(params['start']).toUtc();
+  if (params.containsKey('end'))
+    end = DateTime.parse(params['end']).toUtc();
 
   HttpResponse res = request.response;
   res.headers.add("Access-Control-Allow-Origin", "*, ");
   res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-  fromMongo().then((data) {
+  fromMongo(constraints, start, end).then((data) {
     print("writing data ...");
     //data.forEach((e) => print(e));
     request.response.statusCode = HttpStatus.OK;
