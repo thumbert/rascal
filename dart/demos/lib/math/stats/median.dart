359174a91d6b9e44812c5fb9cb6769f9da9a7e54
thumbert <humbert.tony@yahoo.com>
2015-05-25 21:55:45 -0400

    median work

--- dart/demos/lib/math/stats/median.dart
+++ dart/demos/lib/math/stats/median.dart
@@ -1,23 +1,93 @@
 library math.stats.median;
 import 'dart:math';
 
+
+/**
+ * See http://en.wikipedia.org/wiki/Quantile
+ */
+enum QuantileType {
+  R1,
+  R2,
+  R3,
+  R4,
+  R5,
+  R6,
+  R7,
+  R8,
+  R9
+}
+
+class OutOfRangeException implements Exception {
+  String message;
+  OutOfRangeException([message]);
+}
+
+/**
+ * No need to do a full sorting Nlog(N).  Partition based selection is a linear-time algorithm.
+ * Caching the pivot, speeds up the search even more (inspired from Apache commons math).
+ */
 class Quantile {
 
   List<Comparable> x;
-  
+
+  /// maximum number of partitioning pivots cached (each level doubles the number of pivots).
+  static final int _MAX_CACHED_LEVELS = 10;
+  List<int> _cachedPivots;
+
   Quantile(List<Comparable> this.x) {
-    x.shuffle();    // recommended by Sedgewick
+    x.shuffle();    // recommended by Sedgewick in case of degeneracy
+    _cachedPivots = new List.filled(pow(2, _MAX_CACHED_LEVELS)-1, -1);
   }
-  
+
+
+  value(num probability) {
+    if (probability >= 0 && probability <= 1)
+      throw new OutOfRangeException('probability needs to be between [0,1]');
+    if (x.length == 0)
+      return double.NAN;
+    if (x.length == 1)
+      return x[0];
+
+
+  }
+
   /**
-   * Rearange the input vector x[lo] to x[hi] and return an integer j such that 
-   * x[lo] to x[j-1] are less than x[j] and x[j+1] to x[hi] are higher than x[j]. 
-   * The pivot element x[j] is chosen as the x[lo] of the original array. 
+   * Calculate the minimum value.  One traversal.
    */
-  int _partition(int lo, int hi) {
+  min() {
+
+  }
+
+
+  /**
+   * Return the k-th smallest element of the array.  The input argument [k] is
+   * between 0 and [x.length]-1.  So if k=0, you return the array minimum.
+   */
+  minK(int k) {
+    assert(k <= x.length-1);
+    x.shuffle();    // recommended by Sedgewick  TODO:  do I need it here too?
+    int lo = 0,
+    hi = x.length - 1;
+    while (hi > lo) {
+      int j = _partition(lo, hi, lo);
+      if (j == k) return x[k];      // done
+      else if (j > k) hi = j - 1;
+      else if (j < k) lo = j + 1;
+    }
+
+    return x[k];
+  }
+
+  /**
+   * Rearrange the input vector x[lo] to x[hi] and return an integer j (pivot) such that
+   * x[lo] to x[j-1] are less than x[j] and x[j+1] to x[hi] are higher than x[j].
+   * The pivot is the initial index of the pivot element.
+   *
+   */
+  int _partition(int lo, int hi, int pivot) {
     int i = lo,
-        j = hi + 1;
-    var v = x[lo];
+    j = hi + 1;
+    var v = x[pivot];
     while (true) {
       while (x[++i].compareTo(v) < 0) if (i == hi) break;
       while (v.compareTo(x[--j]) < 0) if (j == lo) break;
@@ -27,36 +97,7 @@ class Quantile {
     _swap(lo, j);
     return j;
   }
-  
-  value(num probability) {
-    
-  }
-  
-  /**
-   * Calculate the minimum value.  One traversal. 
-   */
-  min() {
-    
-  }
-  
-  
-  /**
-   * Return the k-th smallest element of the array.  The input argument [k] is 
-   * between 0 and [x.length]-1.  So if k=0, you return the array minimum.
-   */
-  minK(int k) {
-    x.shuffle();    // recommended by Sedgewick
-    int lo = 0,
-        hi = x.length - 1;
-    while (hi > lo) {
-      int j = _partition(lo, hi);
-      if (j == k) return x[k];      // done
-      else if (j > k) hi = j - 1;
-      else if (j < k) lo = j + 1;
-    }
-    
-    return x[k];
-  }
+
 
   void _sortLoHi(int lo, int hi) {
     if (hi <= lo) return;
@@ -64,72 +105,15 @@ class Quantile {
     _sortLoHi(lo, j-1);
     _sortLoHi(j+1, hi);
   }
-  
+
   void _swap(int i, int j) {
     var t = x[i];
     x[i] = x[j];
     x[j] = t;
   }
-  
+
   sort() => _sortLoHi(0, x.length-1);
-  
-  
+
+
 }
 
-
-
-
-/**
- * Find the n-th smallest element of the numerical List x
- */
-num quantile_bad(List<num> x, int n) {
-  int low, high;
-  int median;
-  int middle, ll, hh;
-
-  void swap(int i, int j) {
-    num t = x[i];
-    x[i] = x[j];
-    x[j] = t;
-  }
-
-  low = 0;
-  high = n - 1;
-  while (true) {
-    if (high <= low + 1) {
-      /// two elements only
-      if (high == low + 1 && x[low] > x[high]) swap(low, high);
-      return x[n - 1];
-    } else {
-
-      // find median of low, middle and high items; swap into position low
-      middle = (low + high) ~/ 2;
-      // swap low item (now in position middle) into position (low+1)
-      swap(middle, low + 1);
-
-      if (x[low] > x[high]) swap(low, high);
-      if (x[middle] > x[high]) swap(middle, high);
-      if (x[middle] > x[low]) swap(middle, low);
-
-
-      // nibble from each end towards middle, swapping elements when stuck
-      ll = low + 1;
-      hh = high;
-      while (true) {
-        do ll++; while (x[low] > x[ll]);
-        do hh--; while (x[hh] > x[low]);
-
-        if (hh < ll) break;
-
-        swap(ll, hh);
-      }
-
-      // swap middle item (in position low) back into correct position
-      swap(low, hh);
-
-      // reset active partition
-      if (hh <= median) low = ll;
-      if (hh >= median) high = hh - 1;
-    }
-  }
-}
--- dart/demos/test/math/stats/test_median.dart
+++ dart/demos/test/math/stats/test_median.dart
@@ -1,7 +1,24 @@
 library test_median;
 
-import 'package:unittest/unittest.dart';
+import 'package:test/test.dart';
 import 'package:demos/math/stats/median.dart';
+
+
+testNumericQuantile() {
+  List x = [3, 1, 5, 9, 4, 2, 7];
+  Quantile q = new Quantile(x);
+  test('min', () {
+    var res = new List.generate(7, (i) => i).map((e) => q.minK(e));
+    expect(res, [1,2,3,4,5,7,9]);
+  });
+
+  var y = [3, 1, double.NAN, 8]..sort();
+  print(y);
+
+}
+
+
+
 
 
 main() {
@@ -16,7 +33,10 @@ main() {
   print(q.x);
   
   print( q.minK(15) );
-  
+
+  var y = 0x1 << 10;
+  print(y);
+
 //  test("k-th min element", () {
 //    var res = [0, 3, 5, 15].map((k) => q.minK( k )).toList();
 //    expect(res, ['A', 'E', 'K', 'X']);
