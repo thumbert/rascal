1f426c3931b11f9f245d60bdea3a0c9f8e0b1406
thumbert <humbert.tony@yahoo.com>
2015-07-27 22:19:54 -0400

    custom iterator example

---
+++ dart/demos/lib/collection/custom_iterator2.dart
@@ -0,0 +1,45 @@
+library collection.custom_iterator2;
+
+import 'dart:collection';
+
+class Square {
+  int n;
+  Square(int this.n);
+}
+
+
+class ChessBoard extends Object with IterableMixin<Square> {
+  List<Square> data;
+
+  ChessBoard(){
+    data = new List.generate(64, (n) => new Square(n));
+  }
+
+  Iterator<Square> get iterator => new ChessBoardIterator(this);
+}
+
+
+
+class ChessBoardIterator extends Iterator<Square> {
+  Square _current;
+  ChessBoard board;
+
+  ChessBoardIterator(ChessBoard this.board){
+    //_current = board.data.first;  // Should I have this here??
+  }
+
+  Square get current => _current;
+
+  bool moveNext() {
+    bool res = true;
+    int n = _current.n;
+    if (n < 64) {
+      _current = board.data[n+1];
+    } else {
+      res = false;
+    }
+
+    return res;
+  }
+
+}
