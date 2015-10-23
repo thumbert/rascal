50bfda42212039ee068ee573f8773d5d3c950828
thumbert <humbert.tony@yahoo.com>
2015-10-22 22:25:12 -0400

    cleaned some mongo

--- dart/demos/test/db/demo_mongo.dart
+++ dart/demos/test/db/demo_mongo.dart
@@ -8,159 +8,170 @@ import 'package:mongo_dart/mongo_dart.dart';
 
 // http://openmymind.net/mongodb.pdf
 
-var data = [{
-    'name': 'Aurora',
-    'gender': 'f',
-    'weight': 450
-}, {
-    'name': 'Barty',
-    'gender': 'm',
-    'weight': 550
-} ];
+unicornsTest() async {
+  var data = [
+    {'name': 'Aurora', 'gender': 'f', 'weight': 450},
+    {'name': 'Barty', 'gender': 'm', 'weight': 550}
+  ];
 
-
-main() {
   print("Original data:");
   print(data);
 
   Db db = new Db('mongodb://127.0.0.1/test');
   DbCollection coll;
 
-  db.open().then((_) {
-    print("\nInsert original data (2 unicorns) in the db.");
-    coll = db.collection("unicorns");
-    coll.remove(); // clean up previous mess
+  await db.open();
+  print("\nInsert original data (2 unicorns) in the db.");
+  coll = db.collection("unicorns");
+  await coll.remove(); // clean up previous mess
 
-    print("Force the index (the unicorn name) to be unique.");
-    /** This guarantees that if you try to insert a unicorn with
+  print("Force the index (the unicorn name) to be unique.");
+  /** This guarantees that if you try to insert a unicorn with
      *  the same name as an already existing unicorn in the db,
      *  the insertion will fail.
      */
-    db.ensureIndex('unicorns', keys: {
-        'name': 1
-    }, unique: true);
+  db.ensureIndex('unicorns', keys: {'name': 1}, unique: true);
 
-    data.forEach((uni) {
-      coll.insert(uni);
-    });
+  data.forEach((uni) {
+    coll.insert(uni);
+  });
 
+  int v = await coll.count();
+  if (v == data.length) {
+    print("\n${data.length} documents inserted OK.");
+  }
 
-  }).then((_) {
-    return coll.count().then((v) {
-      if (v == data.length) {
-        print("\n${data.length} documents inserted OK.");
-      }
-    });
+  print('\nCalling unicorn Barty for inspection:');
+  List res = await coll.find({'name': 'Barty'}).toList();
+  print(res);
 
+  print("\nFind a unicorn that doesn't exist (looking for Catherina)!");
+  List cath = await coll.find({'name': 'Catherina'}).toList();
+  if (cath.isNotEmpty) throw 'should not get here';
 
-  }).then((_) {
-    print('\nCalling unicorn Barty for inspection:');
-    return coll.find({
-        'name': 'Barty'
-    }).toList().then((e) {
-      print(e);
-    });
+  print('\nInserting another unicorn with name Aurora (should not be allowed)');
+  await coll.insert({'name': 'Aurora', 'gender': 'f', 'weight': 330}).then((_) {
+  }, onError: (e) => print(e));
 
-
-  }).then((_) {
-    print("\nFind a unicorn that doesn't exist (looking for Catherina)!");
-    coll.find({
-        'name': 'Catherina'
-    }).toList().then((e) {
-      //it does not get here EVER!
-      print("Found $e");
-    });
-    return coll.count({
-        'name': 'Catherina'
-    }).then((v) {
-      // it doesn't get here EVER!
-      print("There are $v unicorns called Catherina!");
-    });
-
-
-  }).then((_) {
-    print('\nInserting another unicorn with name Aurora (should not be allowed)');
-    return
-      coll.insert({
-          'name': 'Aurora', 'gender': 'f', 'weight': 330
-      }).then((_) {
-      }, onError: (e) => print(e));
-
-
-  }).then((_) {
-    /**
+  /**
      * What happens when you insertAll and one of the entries has a conflict?
      * Devon makes it (no conflict), Aurora triggers an error and the execution stops.
      * Ellie doesn't make it in the db.
      */
-    var aux = [{
-        'name': 'Devon', 'gender': 'm', 'weight': 222
-    }, {
-        'name': 'Aurora', 'gender': 'f', 'weight': 330
-    }, {
-        'name': 'Ellie', 'gender': 'f', 'weight': 440
-    }];
-    print('\nInserting ${aux.length} unicorns with insertAll (Aurora should conflict): \n$aux.');
-    return coll.insertAll(aux).then((_) {
-    }, onError: (e) => print(e));
+  var aux = [
+    {'name': 'Devon', 'gender': 'm', 'weight': 222},
+    {'name': 'Aurora', 'gender': 'f', 'weight': 330},
+    {'name': 'Ellie', 'gender': 'f', 'weight': 440}
+  ];
+  print(
+      '\nInserting ${aux.length} unicorns with insertAll (Aurora should conflict): \n$aux.');
+  await coll.insertAll(aux).then((_) {}, onError: (e) => print(e));
 
+  int i = await coll.count();
+  print('\nThere are now $i documents in the collection.');
 
-  }).then((_) {
-    return coll.count().then((v) {
-      print('\nThere are now $v documents in the collection.');
-    });
+  await coll.find().toList().then((List v) {
+    var names = v.map((e) => e['name']).join(', ');
+    print('Names of unicorns in db are: $names.  Ellie didn\'t make it.');
+  });
 
+  print('\nInserting Cathy, Ellie and Frank');
+  var aux2 = [
+    {'name': 'Cathy', 'gender': 'f', 'weight': 2500},
+    {'name': 'Frank', 'gender': 'm', 'weight': 111},
+    {'name': 'Ellie', 'gender': 'f', 'weight': 440}
+  ];
+  await coll.insertAll(aux2).then((_) {}, onError: (e) => print(e));
 
-  }).then((_) {
-    return coll.find().toList().then((List v){
-      var names = v.map((e) => e['name']).join(', ');
-      print('Names of unicorns in db are: $names.  Ellie didn\'t make it.');
-    });
+  var fattie = await coll
+      .find(where.sortBy('weight', descending: true).limit(1))
+      .toList();
+  print('\nFind the fattest unicorn of them all (should be Cathy!):\n$fattie');
 
-
-  }).then((_) {
-    print('\nInserting Cathy, Ellie and Frank');
-    var aux = [{
-        'name': 'Cathy', 'gender': 'f', 'weight': 2500
-    }, {
-        'name': 'Frank', 'gender': 'm', 'weight': 111
-    }, {
-        'name': 'Ellie', 'gender': 'f', 'weight': 440
-    }];
-    return coll.insertAll(aux).then((_) {
-    }, onError: (e) => print(e));
-
-
-  }).then((_) {
-    return coll.find(where.sortBy('weight', descending: true).limit(1)).toList().then((v) {
-      print('\nFind the fattest unicorn of them all:\n$v');
-    });
-
-
-  }).then((_) {
-    /**
+  /**
      * see: https://github.com/vadimtsushko/mongo_dart/blob/master/test/database_tests.dart
      * Aggregation pipeline example.
      * Find the max weight by gender
-     db.unicorns.aggregate([
-      {$group: {_id: '$gender', maxWeight: {$max: '$weight'}}}
-     ])
-    */
-    List pipeline = [];
-    var group = {'\$group': {'_id': '\$gender', 'maxWeight': {'\$max': '\$weight'}}};
-    pipeline.add( group );
-    return coll.aggregate(pipeline).then((v){
-      print('\nFind the fattest unicorn by gender (Aggregation pipeline):');
-      v['result'].forEach((e) => print(e));
-    });
+        db.unicorns.aggregate([
+        {$group: {_id: '$gender', maxWeight: {$max: '$weight'}}}
+        ])
+     */
+  List pipeline = [];
+  var group = {
+    '\$group': {
+      '_id': '\$gender',
+      'maxWeight': {'\$max': '\$weight'}
+    }
+  };
+  pipeline.add(group);
+  Map agg = await coll.aggregate(pipeline);
+  print('\nFind the fattest unicorn by gender (Aggregation pipeline):');
+  agg['result'].forEach((e) => print(e));
+
+  print('Have an example on how to remove records ...');
+
+  print("\nClosing the db");
+  await db.close();
+}
+
+selectFromArray() async {
+  List grades = [
+    {
+      "_id": 1,
+      "semester": 1,
+      "grades": [70, 87, 90]
+    },
+    {
+      "_id": 2,
+      "semester": 1,
+      "grades": [90, 88, 92]
+    },
+    {
+      "_id": 3,
+      "semester": 1,
+      "grades": [85, 100, 90]
+    },
+    {
+      "_id": 4,
+      "semester": 2,
+      "grades": [79, 85, 80]
+    },
+    {
+      "_id": 5,
+      "semester": 2,
+      "grades": [88, 88, 92]
+    },
+    {
+      "_id": 6,
+      "semester": 2,
+      "grades": [95, 90, 96]
+    }
+  ];
+
+  Db db = new Db('mongodb://127.0.0.1/test');
+  DbCollection coll;
+
+  await db.open();
+  coll = db.collection("grades");
+  await coll.remove();
+  await coll.insertAll(grades);
+
+  print('Select all docs with midterm grades > 89');
+  SelectorBuilder sb = where.gt('grades.1', 89);
+  print(sb.toString());
+  List res = await coll.find(sb).toList();
+  res.forEach((e) => print(e));
 
 
-  }).then((_) {
-    print('Have an example on how to remove records ...');
+  print('Select only the midterm grades for semester 2');
 
 
-  }).then((_) {
-    print("\nClosing the db");
-    return db.close();
-  });
+  //await coll.remove();
+  await db.close();
+}
+
+main() async {
+  //unicornsTest();
+
+  selectFromArray();
 }
--- dart/demos/test/db/demo_mongo_aggregation.dart
+++ dart/demos/test/db/demo_mongo_aggregation.dart
@@ -2,7 +2,6 @@ library demo_mongo_aggregation;
 
 import 'package:mongo_dart/mongo_dart.dart';
 
-
 /**
  * An example of using the aggregation pipeline.  Aggregate timeseries
  * by month and id.
@@ -19,51 +18,45 @@ List<Map> data = [
   {'day': '2015-02-02', 'id': 'B', 'value': 8},
 ];
 
-
-main() {
+main() async {
   Db db = new Db('mongodb://127.0.0.1/test');
   DbCollection coll;
 
-  db.open().then((_) {
-    coll = db.collection("timeseries");
-    return coll.remove();
+  await db.open();
+  coll = db.collection("timeseries");
+  await coll.remove();
 
-  }).then((_) {
-    /// insert the data
-    data.forEach((uni) => coll.insert(uni));
+  /// insert the data
+  data.forEach((uni) => coll.insert(uni));
 
-  }).then((_) {
-    /// aggregate the values by month and id
-    List pipeline = [];
-    var project = {
-      '\$project': {
-        'id': 1,
-        'value': 1,
-        'month': {
-          '\$substr': ['\$day', 0, 7]
-        },
-      }
-    };
-    var group = {
-      '\$group': {
-        '_id': {'month': '\$month', 'id': '\$id'},
-        'avgValue': {'\$avg': '\$value'}
-      }
-    };
+  /// aggregate the values by month and id
+  List pipeline = [];
+  var project = {
+    '\$project': {
+      'id': 1,
+      'value': 1,
+      'month': {
+        '\$substr': ['\$day', 0, 7]
+      },
+    }
+  };
+  var group = {
+    '\$group': {
+      '_id': {'month': '\$month', 'id': '\$id'},
+      'avgValue': {'\$avg': '\$value'}
+    }
+  };
 
-    pipeline.add(project);
-    pipeline.add(group);
-    //print(pipeline);
-    return coll.aggregate(pipeline);
+  pipeline.add(project);
+  pipeline.add(group);
+  //print(pipeline);
+  var res = await coll.aggregate(pipeline);
 
-  }).then((res) {
-    res['result'].forEach((e) => print(e));
+  res['result'].forEach((e) => print(e));
 //    {_id: {month: 2015-02, id: B}, avgValue: 7.5}
 //    {_id: {month: 2015-02, id: A}, avgValue: 5.5}
 //    {_id: {month: 2015-01, id: B}, avgValue: 3.5}
 //    {_id: {month: 2015-01, id: A}, avgValue: 1.5}
 
-  }).then((_) => db.close());
-
-
+  await db.close();
 }
