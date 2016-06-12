be87e1b3653c21dc2df293aeee6f744fcf801431
thumbert <humbert.tony@yahoo.com>
2016-06-11 21:54:46 -0400

    update

--- dart/demos/lib/string/read_sms_backup.dart
+++ dart/demos/lib/string/read_sms_backup.dart
@@ -1,19 +1,64 @@
 library string.sms_backup;
 
 import 'dart:io' show File;
-import 'package:xml/xml.dart' as xml;
+import 'package:xml/xml.dart';
+
+
+class Message {
+  DateTime time;
+  String author;
+  String content;
+
+  Message(this.time, this.author, this.content);
+
+  Message.fromXml(XmlNode xml) {
+    List<XmlAttribute> attrs = xml.attributes;
+    attrs.forEach((XmlAttribute a) {
+      if (a.name.toString() == 'date') time = new DateTime.fromMicrosecondsSinceEpoch(1000*int.parse(a.value));
+      if (a.name.toString() == 'body') content = a.value;
+      if (a.name.toString() == 'contact_name') author = a.value;
+    });
+  }
+
+  /// Convert this message to a Latex chunk
+  String toLatex() {
+    return '';
+  }
+
+  String toString() => '{"time": $time, "author": $author, "content": $content}';
+}
+
+/// Make the document
+void makeLatex(List<Message> x) {
+//\documentclass[11pt,twocolumn]{revtex4-1}
+//
+//\usepackage{color}
+//
+//\begin{document}
+//\title{A textual history}
+//\maketitle
+//
+//
+//Bla bla here
+//
+//\begin{itemize}
+//\item[\color{blue} {\bf A}] He says
+//\item[\color{magenta} {\bf B}] She says
+//\end{itemize}
+//
+//
+//\end{document}
+
+
+}
+
 
 /**
  * Process the xml file.  Usually I drop the file
  */
 String processFile(File file) {
-
   String aux = file.readAsStringSync();
-  var doc = xml.parse(aux);
-
+  var doc = parse(aux);
   StringBuffer res = new StringBuffer();
-
-
-
   return res.toString();
 }
--- dart/demos/test/string/sms_backup.dart
+++ dart/demos/test/string/sms_backup.dart
@@ -1,12 +1,44 @@
 library test.sms_backup;
 
 import 'dart:io';
+import 'package:xml/xml.dart';
 import 'package:demos/string/read_sms_backup.dart';
 
-main() {
-  print(Directory.current);
-  File file = new File('tmp/sms-20160412220202.xml');
-  String res = processFile(file);
 
-  print(res);
+List<String> data = [
+  """<sms protocol="0" address="+12222222222" date="1464453545899" type="1" subject="null" body="I have just enough shade to do a little more but I'm afraid to do damage." toa="null" sc_toa="null" service_center="6502531234" read="1" status="-1" locked="0" date_sent="1464453545000" readable_date="May 28, 2016 12:39:05 PM" contact_name="C" />"""
+];
+
+
+
+List<Message> readFile(File filename) {
+  File file = new File('~/Downloads/sms-20160609063913.xml');
+  String aux = file.readAsStringSync();
+  XmlDocument doc = parse(aux);
+
+  List<Message> res = [];
+
+
+
+  return res;
+}
+
+
+
+/// Create the Latex file
+void writeLatex() {
+
+}
+
+
+main() {
+
+//  XmlDocument res = parse(data.first);
+//  var x = res.firstChild;
+//  Message msg = new Message.fromXml(x);
+//  print(msg);
+
+
+
+  //print(res);
 }
