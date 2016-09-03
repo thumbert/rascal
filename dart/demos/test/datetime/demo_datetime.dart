7703e2f494f28fb6a8e2b09aea4b3f7af58baaf2
thumbert <humbert.tony@yahoo.com>
2016-09-03 12:07:30 -0400

    example parsing datetime

--- dart/demos/test/datetime/demo_datetime.dart
+++ dart/demos/test/datetime/demo_datetime.dart
@@ -1,34 +1,48 @@
-library format_datetime;
+library test.format_datetime;
+
 import 'package:intl/intl.dart';
+import 'package:test/test.dart';
+import 'package:timezone/standalone.dart';
+
+
+Location location = getLocation('US/Eastern');
 
 /**
  * Example of parsing different formats
  */
 parsingDateTimes() {
 
-  var dt2 = DateTime.parse("2014-01-01 09:00:00");
-  print(dt2.toString());   // 2014-01-01 09:00:00.000
-  //print(dt2.isUtc);        // false
-  //print(dt2.timeZoneName); // "EST"
+  test('parse 2014-01-01 09:00:00 directly', () {
+    var dt2 = DateTime.parse("2014-01-01 09:00:00");
+    expect(dt2.toString(), '2014-01-01 09:00:00.000');
+    //print(dt2.isUtc);        // false
+    //print(dt2.timeZoneName); // "EST"
+  });
 
-  var fmt1 = new DateFormat("yyyy-MM-ddTHH");
-  print(fmt1.parse("2015-01-01T10").toString() == "2015-01-01 10:00:00.000");
+//  var fmt1 = new DateFormat("yyyy-MM-ddTHH");
+//  print(fmt1.parse("2015-01-01T10").toString() == "2015-01-01 10:00:00.000");
+//
+//  var fmt2 = new DateFormat("yyyy-MM-ddTHH:mm:ss");
+//  print(fmt2.parse("2015-01-01T10:12:34").toString() == "2015-01-01 10:12:34.000");
 
-  var fmt2 = new DateFormat("yyyy-MM-ddTHH:mm:ss");
-  print(fmt2.parse("2015-01-01T10:12:34").toString() == "2015-01-01 10:12:34.000");
+  test('parse yyyy-MM-ddTHH:mm:ss.SSS', (){
+    var fmt3 = new DateFormat("yyyy-MM-ddTHH:mm:ss.SSS");
+    expect(fmt3.parse("2015-01-01T10:12:34.389").toString(), "2015-01-01 10:12:34.389");
+  });
 
-  var fmt3 = new DateFormat("yyyy-MM-ddTHH:mm:ss.SSS");
-  print(fmt3.parse("2015-01-01T10:12:34.389").toString() == "2015-01-01 10:12:34.389");
-
-  // not working as Dart does not understand offsets.  Everything gets parsed into local time!
-  var fmt4 = new DateFormat("yyyy-MM-ddTHH:mm:ss.SSS-ZZZZ");
-  print(fmt4.parse("2015-01-01T10:00:00.000-0600",false) == new DateTime(2015,1,1,10));
-  print(fmt4.parse("2015-01-01T10:00:00.000-1000",false) == new DateTime(2015,1,1,10));
+  test('parse yyyy-MM-ddTHH:mm:ss.SSS-ZZZZ with offset is parsed by TZDateTime.parse()', (){
+    // not working with intl package Dart does not understand offsets.
+    // Everything gets parsed into local time!
+    TZDateTime dt = TZDateTime.parse(location, '2015-03-30T10:00:00.000-04:00');
+    expect(dt.timeZoneOffset, new Duration(hours: -4));
+    expect(dt.hour, 10);
+  });
 
 
   //print(fmt1.parse("2015-01-01T00:00:00.000-05:00"));
 
 }
+
 
 formatDateTimes() {
   DateFormat fmt = new DateFormat('yyyyMMdd');
@@ -38,8 +52,13 @@ formatDateTimes() {
 
 
 main() {
+  initializeTimeZoneSync();
 
   parsingDateTimes();
+
+//  var fmt4 = new DateFormat("yyyy-MM-ddTHH:mm:ss.SSS-ZZZZ");
+//  print(fmt4.parse("2015-01-01T10:00:00.000-0600",false));
+
 
 
 //  var dt = new DateTime(2014,1,1,9);
