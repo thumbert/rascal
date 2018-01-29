00d2994b839a8965da901c4af305ba8cefb042bc
thumberts <tony.humberts@gmail.com>
2018-01-28 23:05:58 -0500

    a working version of a term parser

--- dart/demos/test/pkg_petitparser/term.dart
+++ dart/demos/test/pkg_petitparser/term.dart
@@ -3,33 +3,22 @@ library term;
 import 'package:petitparser/petitparser.dart';
 import 'package:date/date.dart';
 
-class Term {
-  Date start;
-  Date end;
+Set<String> mon = new Set.from([
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+]);
 
-  static Set<String> mon = new Set.from([
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec'
-  ]);
-
-  Term(this.start, this.end);
-
-  Term.parse(String x) {}
-
-  _parseCompound(String x) {}
-}
-
-Map _monIdx = {
+Map _monthIdx = {
   'jan': 1,
   'feb': 2,
   'mar': 3,
@@ -59,37 +48,84 @@ List _monthNames = [
   'december',
 ];
 
-/// Return the month integer or null if it fails
-/// to parse a month name.  Parses 'Jan' or 'january'.
-int _parseMonth(String x) {
-  int res;
-  var key = x.toLowerCase().substring(0, 3);
-  if (_monIdx.containsKey(key)) {
-    res = _monIdx[key];
-    /// if you pass the full month name, you have to spell correctly
-    if (x.length > 3 && x != _monthNames[res])
-      res = null;
+class Term {
+  Date start;
+  Date end;
+
+  Term(this.start, this.end) {
+    if (end.isBefore(start))
+      throw new ArgumentError('End date can\'t be before start date.');
   }
-  return res;
+
+  Term.parse(String x) {}
 }
 
-Parser _yyParser = digit().repeat(2, 4);
-Parser _mmmParser = letter().repeat(3, 3);
-Parser _monthParser = letter('Jan').or(letter('Feb'));
+class TermGrammar extends GrammarParser {
+  TermGrammar() : super(const TermGrammarDefinition());
+}
+class TermGrammarDefinition extends GrammarDefinition {
+  const TermGrammarDefinition();
 
-Parser _mmmyyParser = _mmmParser.seq(_yyParser).flatten();
+  start() => ref(value).end();
+  token(Parser p) => p.flatten().trim();
+  simpleDayToken() => ref(dayToken) & ref(monthToken) & ref(yearToken);
+  simpleMonthToken() => ref(monthToken) & ref(yearToken);
+  simpleToken() => ref(simpleMonthToken) | ref(simpleDayToken);
 
-Parser simpleTermParser = letter().plus() & digit().repeat(2, 4);
-Parser compositeTermParser = simpleTermParser.seq(char('-')).seq(simpleTermParser);
+  compoundDayToken() =>
+      ref(simpleDayToken) & ref(token, char('-')) & ref(simpleDayToken);
+  compoundMonthToken() =>
+      ref(simpleMonthToken) & ref(token, char('-')) & ref(simpleMonthToken);
+  compoundToken() => compoundMonthToken() | compoundDayToken();
+  value() => ref(simpleToken) | ref(compoundToken);
+
+  dayToken() => token(digit().repeat(1, 2));
+  monthToken() => token(letter().repeat(3, 3));
+  yearToken() => token(digit().repeat(2, 4));
+}
+
+class TermParser extends GrammarParser {
+  /// The parser will fail with hard to understand message if input is not
+  /// correct.
+  TermParser() : super(const TermParserDefinition());
+}
+
+/// the parser definition
+class TermParserDefinition extends TermGrammarDefinition {
+  const TermParserDefinition();
+
+  simpleMonthToken() => super.simpleMonthToken().map((List<String> each) {
+        print(each);
+        Month month = new Month(_yyToYear(each[1]), _monthIdx[each[0].toLowerCase()]);
+        return new Term(month.startDate, month.endDate);
+      });
+  simpleDayToken() => super.simpleDayToken().map((List<String> each) {
+    print(each);
+    Date day = new Date(_yyToYear(each[2]), _monthIdx[each[1].toLowerCase()], int.parse(each[0]));
+    return new Term(day, day);
+  });
+}
+
+/// Convert a two digit string to a year value.
+int _yyToYear(String yy) {
+  int value = int.parse(yy);
+  if (value > 50) return 1900 + value;
+  else return 2000 + value;
+}
+
 
 main() {
-  Parser d2 = digit().repeat(2, 2).end(); // two digits
-  Result r = d2.parse('15');
-  print(r.value);
+//  Parser d2 = digit().repeat(2, 2).end(); // two digits
+//  Result r = d2.parse('15');
+//  print(r.value);
 
-  Result r2 = p3.parse('January17');
-  print(r2.position);
-  print(r2.isSuccess);
+  var parser = new TermParser();
+  var jan17 = parser.parse('jan 17');
+  print(jan17.value);
 
-  List x = [];
+  var d1jan17 = parser.parse('1 jan 17');
+  print(d1jan17.value);
+
+  //parser.parse('jav 17');  // blows-up
+
 }
