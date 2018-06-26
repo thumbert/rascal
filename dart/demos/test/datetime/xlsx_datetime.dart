06711af5ac4dd6ea8a85a73dc89460eebbbd7fc5
thumbert <tony.humberts@gmail.com>
2018-06-26 14:44:38 -0400

    Update xlsx_datetime.dart

--- dart/demos/test/datetime/xlsx_datetime.dart
+++ dart/demos/test/datetime/xlsx_datetime.dart
@@ -4,3 +4,9 @@ DateTime convertXlsxDateTime(num x) =>
     new DateTime.fromMillisecondsSinceEpoch(((x - 25569) * 86400000).round(),
         isUtc: true);
 
+/// 
+Date convertXlsxDate(num x, {Location location}) {
+  var aux = new DateTime.fromMillisecondsSinceEpoch(((x - 25569) * 86400000).round(),
+      isUtc: true);
+  return new Date(aux.year, aux.month, aux.day, location: location);
+}
